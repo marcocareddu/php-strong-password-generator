be06d8984002ad6055ed92ba1947146ee1f2209a
marcocareddu <saldumaccu@tuta.io>
2023-07-19 18:28:47 +0200

    PHP - Noved data in characters.php

---
+++ includes/data/characters.php
@@ -0,0 +1,8 @@
+<?php
+// Array 
+
+// Create alphabet variables
+$uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$lowercase = 'abcdefghijklmnopqrstuvwxyz';
+$numbers = '0123456789';
+$symbols = '!@#$%`&*';
